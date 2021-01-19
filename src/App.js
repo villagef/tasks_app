456cd797172a0e4fef7093ea75f6e11e5aba7de6
villagef <filwyd123@gmail.com>
2021-01-19 20:28:48 +0100

    get input value from input and add unique id

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
 import './App.css';
+import {useState, useEffect} from 'react';
+
 
 function App() {
+  const [inputValue, setInpuValue] = useState('');
+  const [tasks, setTasks] = useState([]);
+  let randomId = Math.random() * 1000; 
+  console.log(tasks);
+  
+  
+
+
+  const handleInput = (e) => {
+    setInpuValue(e.target.value);
+    console.log(e.target.value);
+    
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTasks([...tasks, {text: inputValue, isDone: false, id: randomId}]);
+    setInpuValue('')
+  }
+
   return (
     <div className="App">
+      <h1>Lista zadań</h1>
+      <div className="listsWrapper">
+        <div className="col-right">
+          
+        </div>
+        <div className="col-left">
+
+        </div>
+        <div className="inputWrapper">
+        <form>
+            <input value={inputValue} onChange={handleInput} type="text" className="input"/>
+            <button onClick={handleSubmit} type="submit" className="button">
+                Dodaj zadanie
+            </button>
+        </form>
+        </div>
+      </div>
 
     </div>
   );
