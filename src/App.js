e27df3ee14e84a5e713e8b8a65e0fcd719bc445f
villagef <filwyd123@gmail.com>
2021-01-19 23:29:57 +0100

    local storage

--- src/App.js
+++ src/App.js
@@ -1,58 +1,70 @@
-import './App.css';
-import {useState, useEffect} from 'react';
+import "./App.css";
+import { useState, useEffect } from "react";
+import useStickyData from './useStickyData'
+
+
 
 function App() {
-  const [inputValue, setInpuValue] = useState('');
-  const [todoTasks, setTodoTasks] = useState([]);
+  const [inputValue, setInpuValue] = useState("");
+  const [todoTasks, setTodoTasks] = useStickyData([]);
+  const [doneTasks, setDoneTasks] = useState([]);
   const [isDone, setIsDone] = useState(false);
-  let randomId = Math.random() * 1000; 
-
-  
-  
-
+  let randomId = Math.random() * 1000;
 
   const handleInput = (e) => {
     setInpuValue(e.target.value);
     console.log(e.target.value);
-    
-  }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setTodoTasks([...todoTasks, {text: inputValue, isDone: isDone, id: randomId}]);
-    setInpuValue('')
-  }
+    setTodoTasks([
+      ...todoTasks,
+      { text: inputValue, isDone: isDone, id: randomId },
+    ]);
+    setInpuValue("");
+  };
+
+  const handleCheck = (e) => {
+    console.log(e.target.id);
+  };
+
+  console.log(todoTasks);
 
   return (
     <div className="app">
       <h1>Lista zadań</h1>
       <div className="listsWrapper">
-        <div className="col-right">
+        <div className="col-left">
+          <h3>Do zrobienia ({todoTasks.length})</h3>
           <ul>
-            {
-              todoTasks.map(task => (
+            {todoTasks.map((task) => (
               <li key={task.id}>
                 <label>
-                    <input type="checkbox" value={task.text}  />
-                    {task.text.toUpperCase()}
+                  <input type="checkbox" onChange={handleCheck} id={task.id} />
+                  {task.text.toUpperCase()}
                 </label>
               </li>
-              ))
-            }
+            ))}
           </ul>
         </div>
-        <div className="col-left">
-
+        <div className="col-right">
+          <h3>Zrobione ({doneTasks.length})</h3>
         </div>
       </div>
       <div className="inputWrapper">
         <form>
-            <input value={inputValue} onChange={handleInput} type="text" className="input"/>
-            <button onClick={handleSubmit} type="submit" className="button">
-                Dodaj zadanie
-            </button>
+          <input
+            value={inputValue}
+            onChange={handleInput}
+            type="text"
+            className="input"
+          />
+          <button onClick={handleSubmit} type="submit" className="button">
+            Dodaj zadanie
+          </button>
         </form>
-        </div>
+      </div>
     </div>
   );
 }
